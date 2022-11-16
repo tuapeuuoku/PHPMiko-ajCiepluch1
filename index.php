e2f818df55f027ea5bff53672523c6734bc8e99d
Traper <traperrokukurwa@wp.pl>
2022-11-16 14:39:10 +0100

    login and register

--- index.php
+++ index.php
@@ -2,7 +2,18 @@
 require_once('class/User.class.php');
 
 $user = new User('jkowalski', 'tajneHasło');
+/*
+if($user->register()) {
+    echo "Zarejestrowano poprawnie";
+} else {
+    echo "Błąd rejestracji użytkownika";
+}
+*/
 
-echo '<pre>';
-var_dump($user);
+if($user->login()) {
+    echo "Zalogowano poprawnie";
+} else {
+    echo "Błędny login lub hasło";
+}
+
 ?>
