ba3b344e5a326b38375b6524554df732f6709d62
Traper <traperrokukurwa@wp.pl>
2022-11-16 13:28:08 +0100

    1

---
+++ User.class.php
@@ -0,0 +1,10 @@
+<?php
+class User {
+    private int $id;
+    private string $login;
+    private string $password;
+    private string $firstname;
+    private string $lastname;
+    
+    public function __
+}
