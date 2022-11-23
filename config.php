37d00152f64668cd76c99145043f7507b87f0ef5
Traper <traperrokukurwa@wp.pl>
2022-11-23 13:36:48 +0100

    composer

--- config.php
+++ config.php
@@ -1,4 +1,12 @@
 <?php
-$db = new mysqli('localhost', 'root', '', 'loginform');
+require_once ('vendor/autoload.php');
+
+$loader = new Twig\Loader\FilesystemLoader('templates');
+
+$twig = new Twig\Environment($loader);
+
+
+$db = new mysqli('localhost', 'root', '', 'loginscheme');
+
 
 ?>
