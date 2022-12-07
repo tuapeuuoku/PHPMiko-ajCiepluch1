749835d7c06afabb1d5232a2f31ddc306611e5f5
Traper <traperrokukurwa@wp.pl>
2022-12-07 14:00:06 +0100

    route1

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
 <?php
+
+use Steampixel\Route;
 require_once('config.php');
 require_once('class/User.class.php');
 
-$user = new User('jkowalski', 'tajneHasło');
-/*
-if($user->register()) {
-    echo "Zarejestrowano poprawnie";
-} else {
-    echo "Błąd rejestracji użytkownika";
-}
-*/
-
-if($user->login()) {
-    echo "Zalogowano poprawnie";
-} else {
-    echo "Błędny login lub hasło";
-}
-
+Route::add('/', function() {
+    echo "Strona główna";
+});
+Route::run('/PHPMikołajCiepluch');
 ?>
